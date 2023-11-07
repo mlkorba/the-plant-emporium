b4b2e5140d4d3b9b852ad9838067fd383d1f2c1e
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-11-07 12:17:19 -0700

    added banner section to page

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import { Navbar } from "@/components/Navbar";
+import { Banner } from "@/components/Banner";
 
 export default function Home() {
   return (
     <>
       <main>
         <Navbar />
+        <Banner />
       </main>
     </>
   );
