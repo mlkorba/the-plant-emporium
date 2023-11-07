3c30be89a4cca05c13a9b47c950ed1c0368b36ab
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-11-07 12:21:26 -0700

    changed icons and added navBarHover class

--- components/Navbar.js
+++ components/Navbar.js
@@ -34,46 +34,32 @@ export const Navbar = () => {
             <p className='text-[14px] -mt-2'>Favourites</p>
           </div> */}
           {/* My Favourites End */}
+          {/* Accounts Start */}
+          <div className='flex flex-col justify-center items-center gap-2 navBarHover'>
+            <BsFillPersonFill className='text-2xl' />
+
+            <p className='text-[14px] -mt-2'>Account</p>
           </div>
-          <h2 className="text-base font-semibold ">Plant Care</h2>
+          {/* Cart Start */}
+          <div className='flex flex-col justify-center items-center gap-2 navBarHover'>
+            <FaShoppingCart className='text-2xl' />
+            <p className='text-[14px] -mt-2'>$0.00</p>
+            <span className='absolute w-4 h-4 bg-red-600 text-white top-12 right-[500px] rounded-full flex items-center justify-center text-xs'>
+              0
+            </span>
         </div>
-        {/* Services End */}
         {/* Search Bar Start */}
-        <div className="h-10 flex flex-1 relative">
+          <div className='h-10  flex relative'>
           <input
-            type="text"
-            className="h-full w-full rounded-full px-4 text-jungleGreen text-base outline-none border-[1px] border-transparent focus-visible:border-jungleGreen duration-200"
-            placeholder="Search"
+              type='text'
+              className='h-full w-[175px] rounded-full px-4 text-jungleGreen text-base outline-none border-[3px] border-transparent focus-visible:border-lavender duration-200'
+              placeholder='Search'
           />
-          <span className="absolute w-8 h-8 rounded-full flex items-center justify-center top-1 right-1 text-jungleGreen text-xl hover:scale-125 transition duration-500 cursor-pointer">
+            <span className='absolute w-8 h-8 rounded-full flex items-center justify-center top-1 right-1 text-jungleGreen text-xl hover:scale-125 transition duration-500 cursor-pointer'>
             <IoIosSearch />
           </span>
         </div>
         {/* Search Bar End */}
-        {/* Cart Start */}
-        <div className="navBarHover">
-          <AiOutlineHeart />
-          <div className="flex-col">
-            <h2 className="text-base font-semibold">My Favourites</h2>
-          </div>
-        </div>
-        {/* Cart End */}
-        {/* Accounts Start*/}
-        <div className="navBarHover">
-          <FaRegUser />
-          <div className="flex-col">
-            <p className="text-xs">Sign In</p>
-            <h2 className="text-base font-semibold -mt-1">Account</h2>
-          </div>
-        </div>
-        {/* Accounts End*/}
-        {/* Cart Start*/}
-        <div className="flex flex-col justify-center items-center gap-2 h-12 px-4 rounded-full bg-transparent hover:bg-jungleGreen duration-300 relative">
-          <BsCart2 className="text-2xl" />
-          <p className="text-[10px] -mt-2">$0.00</p>
-          <span className="absolute w-4 h-4 bg-red-600 text-white top-0 right-4 rounded-full flex items-center justify-center text-xs">
-            0
-          </span>
         </div>
         {/* Cart End*/}
       </div>
