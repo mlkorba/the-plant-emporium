5ec7443993529d420bf3c5d1b0aff9f08d2b8cad
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-11-07 12:21:42 -0700

    adjusted container max width

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,16 +7,19 @@ import { FaShoppingCart } from 'react-icons/fa'
 
 export const Navbar = () => {
   return (
-    <div className="w-full bg-oliveGreen text-white">
-      <div className="max-w-container mx-auto h-20 px-4 flex items-center gap-2 justify-between">
-        {/* Logo Start */}
-        <div>
-          <Image
-            src={logo}
-            alt="logo"
-            className="w-24 hover:scale-125 transition duration-500 cursor-pointer"
-          />
-        </div>
+    <div className='w-full text-white'>
+      <div className='max-w-[1280px] mt-10 mx-auto h-20 px-4 flex items-center justify-between'>
+        {/* Left section with logo, department, and services */}
+        <div className='flex items-center gap-2'>
+          {/* Logo Start */}
+          <div>
+            <Image
+              src={logo}
+              alt='logo'
+              className='w-[100px] hover:scale-125 transition duration-500 cursor-pointer'
+            />
+          </div>
+
           <div>
             <ul className='flex items-center gap-2 '>
               <li className='text-base font-semibold navBarHover'>Shop</li>
@@ -27,6 +30,8 @@ export const Navbar = () => {
         <div className='text-[40px] font-bold'>
           <h2>The Plant Emporium</h2>
         </div>
+
+        {/* Right section with other elements */}
         <div className='flex items-center gap-4'>
           {/* My Favourites Start */}
           {/* <div className='flex flex-col justify-center items-center gap-2 navBarHover'>
@@ -47,22 +52,21 @@ export const Navbar = () => {
             <span className='absolute w-4 h-4 bg-red-600 text-white top-12 right-[500px] rounded-full flex items-center justify-center text-xs'>
               0
             </span>
-        </div>
-        {/* Search Bar Start */}
+          </div>
+          {/* Search Bar Start */}
           <div className='h-10  flex relative'>
-          <input
+            <input
               type='text'
               className='h-full w-[175px] rounded-full px-4 text-jungleGreen text-base outline-none border-[3px] border-transparent focus-visible:border-lavender duration-200'
               placeholder='Search'
-          />
+            />
             <span className='absolute w-8 h-8 rounded-full flex items-center justify-center top-1 right-1 text-jungleGreen text-xl hover:scale-125 transition duration-500 cursor-pointer'>
-            <IoIosSearch />
-          </span>
+              <IoIosSearch />
+            </span>
+          </div>
+          {/* Search Bar End */}
         </div>
-        {/* Search Bar End */}
-        </div>
-        {/* Cart End*/}
       </div>
     </div>
-  );
-};
+  )
+}
