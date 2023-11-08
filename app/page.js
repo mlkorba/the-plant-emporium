6983d3708d6bdc1a6f411a0fac9255acad14c7f2
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-11-07 18:21:25 -0700

    added ProductSection to page

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import { Navbar } from "@/components/Navbar";
 import { Banner } from "@/components/Banner";
+import { ProductSection } from "@/components/ProductSection";
 import { Footer } from "@/components/Footer";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <main>
         <Navbar />
         <Banner />
+        <ProductSection />
       </main>
       <footer>
         <Footer />
