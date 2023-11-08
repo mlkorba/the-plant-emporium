086b9376e712912a1437519f7682be50f6a2fbdc
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-11-07 21:52:23 -0700

    removed search bar and increased h1 text size

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,62 +8,45 @@ import { FaShoppingCart } from 'react-icons/fa'
 export const Navbar = () => {
   return (
     <div className='w-full text-white'>
-  <div className='max-w-container mt-10 mx-auto h-20 px-4 flex items-center justify-between gap-5'>
-    <div className='flex items-center'>
-      <div>
-        <Image
-          src={logo}
-          alt='logo'
-          className='w-[100px] hover:scale-125 transition duration-500 cursor-pointer'
-        />
-      </div>
+      <div className='max-w-container mt-10 mx-auto h-20 px-4 flex items-center justify-between gap-2'>
+        <div className='flex items-center gap-2'>
+          <div>
+            <Image
+              src={logo}
+              alt='logo'
+              className='w-[100px] hover:scale-125 transition duration-500 cursor-pointer'
+            />
+          </div>
 
-      <div>
-        <ul className='flex items-center gap-1'>
-          <li className='text-base font-semibold navBarHover'>Shop</li>
-          <li className='text-base font-semibold navBarHover'>Services</li>
-        </ul>
-      </div>
-    </div> 
+          <div>
+            <ul className='flex items-center gap-1'>
+              <li className='text-base font-semibold navBarHover'>Shop</li>
+              <li className='text-base font-semibold navBarHover'>Services</li>
+            </ul>
+          </div>
+        </div>
 
-    <div className='text-center flex-grow'>
-      <p className='text-lg font-semibold'>The Plant Emporium</p>
+        <div className='text-center flex-grow'>
+          <h1 className='text-[32px] font-bold'>The Plant Emporium</h1>
+        </div>
+
+        <div className='flex items-center justify-end gap-2'>
+          <div className='flex flex-col justify-center items-center gap-0 navBarHover'>
+            <BsFillPersonFill className='text-2xl' />
+          </div>
+
+          <div className='flex flex-col justify-center items-center gap-0 navBarHover'>
+            <FaShoppingCart className='text-2xl' />
+          </div>
+
+          <div className='relative'>
+            <div className='flex flex-col justify-center items-center gap-0 navBarHover'>
+              <IoIosSearch className='text-2xl' />
+            </div>
+
+          </div>
+        </div>
+      </div>
     </div>
-    {/* Company Name */}
-    
-    <div className='flex items-center justify-end'>
-      <div className='flex flex-col justify-center items-center gap-0 navBarHover'>
-        <BsFillPersonFill className='text-2xl' />
-        <p className='text-[14px]'>Account</p>
-      </div>
-
-      <div className='flex flex-col justify-center items-center gap-0 navBarHover'>
-        <FaShoppingCart className='text-2xl' />
-        <p className='text-[14px]'>$0.00</p>
-      </div>
-
-      <div className='relative'>
-  <div className='flex flex-col justify-center items-center gap-0 navBarHover'>
-    <IoIosSearch className='text-2xl' />
-  </div>
-
-  <div className='absolute top-12 right-0 p-2 bg-white rounded-full hidden'>
-    {/* Search bar content */}
-    <div className="flex">
-      <input type='text' className="rounded-full p-2" placeholder='Search...' />
-      <button className="bg-blue-500 text-white rounded-r-lg">Search</button>
-    </div>
-  </div>
-</div>
-
-
-
-      
-    </div>
-    {/* Right Side: Account and Cart */}
-  </div>
-</div>
-
-
   )
 }
