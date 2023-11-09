da442306dbef962677a426bb2319263ac96bb07f
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-11-09 16:48:10 -0700

    designed product card

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,21 +1,37 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import plantOne from '@/public/assets/images/product-images/kari-shea-3_cyj5YkhTs-unsplash.jpg'
 
 
-export const ProductCard = ({ product }) => {
-    return (
-      <div className="max-w-xs rounded overflow-hidden shadow-lg">
-        <img src={product.imageUrl} alt={product.name} className="w-full" />
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">{product.name}</div>
-          <p className="text-gray-700 text-base">Price: ${product.price}</p>
-        </div>
-        <div className="px-6 py-4">
-          <Link href={`/products/${product.id}`}>
-            <a className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-              View Details
-            </a>
-          </Link>
+export const ProductCard = ({ uid, plant_name, image, price, oldPrice }) => {
+  return (
+    <div className='group max-w-[500px] max-h-[350px] rounded-lg shadow-lg overflow-hidden '>
+      <div className='w-full h-1/2 cursor-pointer overflow-hidden'>
+        <Image
+          className='w-full group-hover:scale-125 transition duration-500 cursor-pointer '
+          src={plantOne}
+          alt={plant_name}
+        />
+      </div>
+      <div className='w-full h-auto px-2 py-4 bg-white'>
+        <div className='text-center'>
+          <div>
+            <h2 className='text-base font-bold'>{plant_name}</h2>
+          </div>
+          <div className='text-center mb-4'>
+              <p className='font-semibold'>${price}</p>
+              {/* <p className='line-through'>${oldPrice}</p> */}
+          </div>
+          <div className="text-center ">
+            <Link href={`/products/${uid}`}>
+              <span className="bg-oliveGreen hover:bg-jungleGreen text-white font-bold py-2 px-4 rounded-full cursor-pointer">
+                 View Details
+              </span>
+            </Link>
+         </div>
         </div>
       </div>
-    );
-  };
+    </div>
+  )
+}
