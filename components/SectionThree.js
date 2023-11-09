b5101f5b17c15176860abacbc9f09e1da0514163
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-11-09 16:48:50 -0700

    added section components

---
+++ components/SectionThree.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const SectionThree = () => {
+  return (
+    <div className='py-10 -mt-[500px] h-96 bg-white'>
+      <div className='flex flex-col items-center gap-4'>
+        <h1 className='text-2xl bg-lavender text-white py-2 w-96 text-center -mt-[68px]'>
+          Discover Nature's Bounty
+        </h1>
+        <span className='w-20 h-[3px] bg-jungleGreen'></span>
+        <p className='max-w-[800px] text-oliveGreen text-center'>
+          Nulla consectetur et mollit tempor esse. Eiusmod aliqua ea culpa
+          veniam exercitation irure duis. Labore officia in amet aliquip ut
+          officia id amet. Elit consequat fugiat nisi proident laboris fugiat
+          fugiat officia mollit. Aute excepteur officia ad irure qui aliqua amet
+          fugiat fugiat.
+        </p>
+      </div>
+    </div>
+  )
+}
