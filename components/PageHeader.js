db740c0385d6153d5fc0c3cf5640a4bbf0fdf7fe
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-11-09 16:46:53 -0700

    removed unused PageHeader

--- components/PageHeader.js
+++
@@ -1,8 +0,0 @@
-export function PageHeader ({ title, tagline }) {
-  return (
-    <header className='text-center pt-16'>
-      <h1 className='text-5xl text-slate-600'>{title || 'Page Title'}</h1>
-      <p>{tagline || 'Page Tagline'}</p>
-    </header>
-  )
-}
