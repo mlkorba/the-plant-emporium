a44031907f5a91348df521e122e9ed5505242a14
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-11-13 22:44:57 -0700

    changed lorem text to real text

--- components/SectionOne.js
+++ components/SectionOne.js
@@ -2,18 +2,14 @@ import React from 'react'
 
 export const SectionOne = () => {
   return (
-    <div className='py-10 -mt-[500px] h-96 bg-white'>
+    <div className='mx-auto -mt-[500px] h-[500px] bg-white mb-80 '>
       <div className='flex flex-col items-center gap-4'>
-        <h1 className='text-2xl bg-lavender text-white py-2 w-96 text-center -mt-[68px]'>
+        <h1 className='text-[40px] bg-lavender font-semibold text-white py-[20px] w-[800px] h-28 text-center -mt-[56px]'>
           Discover Nature's Bounty
         </h1>
         <span className='w-20 h-[3px] bg-jungleGreen'></span>
-        <p className='max-w-[800px] text-oliveGreen text-center'>
-          Nulla consectetur et mollit tempor esse. Eiusmod aliqua ea culpa
-          veniam exercitation irure duis. Labore officia in amet aliquip ut
-          officia id amet. Elit consequat fugiat nisi proident laboris fugiat
-          fugiat officia mollit. Aute excepteur officia ad irure qui aliqua amet
-          fugiat fugiat.
+        <p className='max-w-[800px] text-oliveGreen text-center text-base font-semibold'>
+        Welcome to our botanical haven, where we invite you to Discover Nature's Bounty. Immerse yourself in the lush world of plants and greenery as we celebrate the beauty and vitality of the natural kingdom. Our plant shop is more than just a place to find your favorite greens—it's an exploration of the wonders of nature. From expertly curated collections to eco-conscious practices, we are dedicated to providing a verdant experience that goes beyond the ordinary. Join us on this journey of discovery, and let the abundance of nature inspire and enrich your life.
         </p>
       </div>
     </div>
