7151ac151c18fabe8cc30bf5e34d24d39713a20d
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-11-13 22:45:07 -0700

    designed section two

--- components/SectionTwo.js
+++ components/SectionTwo.js
@@ -1,7 +1,31 @@
-// import React from 'react'
+import React from 'react'
+import { TbTruckDelivery } from 'react-icons/tb'
 
-// export const SectionTwo = () => {
-//   return (
-    
-//   )
-// }
+export const SectionTwo = () => {
+  return (
+    <section className='py-10 -mt-[500px] h-80 bg-darkPurple flex gap-28 items-center justify-center mb-16'>
+      <div className='max-w-[1280px] flex justify-center items-center gap-52'>
+        <div className='w-52 h-52 bg-white rounded-full p-4 flex items-center justify-center'>
+          <div className='flex flex-col items-center gap-2'>
+            <TbTruckDelivery className='text-[30px]' />
+            <p>Fast delivery</p>
+          </div>
+        </div>
+
+        <div className='w-52 h-52 bg-white rounded-full p-4 flex items-center justify-center'>
+          <div className='flex flex-col items-center gap-2'>
+            <TbTruckDelivery />
+            <p>Fast delivery</p>
+          </div>
+        </div>
+
+        <div className='w-52 h-52 bg-white rounded-full p-4 flex items-center justify-center'>
+          <div className='flex flex-col items-center gap-2'>
+            <TbTruckDelivery />
+            <p>Fast delivery</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
