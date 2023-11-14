ae75861bd04a72394b00f981a2e7b9d4ec880483
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-11-13 22:44:14 -0700

    removed text from centre

--- components/Banner.js
+++ components/Banner.js
@@ -5,16 +5,14 @@ import { landingBg } from '@/public/assets/images'
 export const Banner = () => {
   return (
     <div className='max-w-full mx-auto'>
-        <div className='bg-black relative -z-1 -mt-[150px] '>
-        <Image
-              src={landingBg}
-              alt='logo'
-              className='w-full h-auto opacity-50'
-            />
-            <div className="absolute top-[450px] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-3xl font-semibold">
-      Your Text Goes Here
-    </div>
-        </div>
-    </div>
+  <div className='bg-black relative -z-1 -mt-10 md:-mt-20 lg:-mt-32'>
+    <Image
+      src={landingBg}
+      alt='logo'
+      className='w-full h-auto opacity-50'
+    />
+  </div>
+</div>
+
   )
 }
