744fe0df9aa5444efc4d5ae9b6e0cb1679062a45
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-11-14 14:45:23 -0700

    changed component name from Banner to Hero

---
+++ components/Hero.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Image from 'next/image'
+import { landingBg } from '@/public/assets/images'
+
+export const Hero = () => {
+  return (
+    <div
+      className="hero-section relative h-screen flex flex-col justify-center text-white"
+      style={{ backgroundImage: `url(${landingBg.src})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
+    >
+      <div className="absolute inset-0 bg-black opacity-40"></div>
+      <div className="hero-content text-center relative z-10 ">
+        <h1 className="text-4xl mb-4">Discover the Beauty of Succulents</h1>
+        <h2 className="text-lg mb-8">Transform your space with our handpicked collection of unique and low-maintenance succulent plants.</h2>
+        <a href="#" className="cta-button bg-jungleGreen text-white py-2 px-4 rounded inline-block text-lg hover:bg-oliveGreen duration-300 cursor-pointer">Explore Our Collection</a>
+      </div>
+    </div>
+  )
+}
