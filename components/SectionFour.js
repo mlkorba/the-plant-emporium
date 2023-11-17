aa18fd739f9ea567db9435a806c2812c303fc901
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-11-17 16:03:23 -0700

    designed Section Four

--- components/SectionFour.js
+++ components/SectionFour.js
@@ -1,21 +1,16 @@
 import React from 'react'
+import Image from 'next/image'
+import { succulentCorner } from '@/public/assets/images'
 
 export const SectionFour = () => {
   return (
-    <div className='py-10 -mt-[500px] h-96 bg-white'>
-      <div className='flex flex-col items-center gap-4'>
-        <h1 className='text-2xl bg-lavender text-white py-2 w-96 text-center -mt-[68px]'>
-          Discover Nature's Bounty
-        </h1>
-        <span className='w-20 h-[3px] bg-jungleGreen'></span>
-        <p className='max-w-[800px] text-oliveGreen text-center'>
-          Nulla consectetur et mollit tempor esse. Eiusmod aliqua ea culpa
-          veniam exercitation irure duis. Labore officia in amet aliquip ut
-          officia id amet. Elit consequat fugiat nisi proident laboris fugiat
-          fugiat officia mollit. Aute excepteur officia ad irure qui aliqua amet
-          fugiat fugiat.
-        </p>
-      </div>
-    </div>
+    <div className="relative">
+  <Image src={succulentCorner} className="w-full" />
+  <div className="absolute top-10 left-5 p-4 text-jungleGreen">
+    <h1 className="text-2xl text-center mb-10">Thank you for bringing a bit of greenery into your life with The Plant Emporium!</h1>
+    <p className='w-1/2'>Your choice to support local succulents means a lot to us and to our community. We're here to make your space a little greener, a little happier. Can't wait to see you again and help you find the perfect succulent companion. Until then, may your days be filled with the beauty of nature. Happy planting!</p>
+  </div>
+</div>
+
   )
 }
