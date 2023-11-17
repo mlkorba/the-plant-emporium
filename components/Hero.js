b41350e7a18259f0c8b0a3aefea222f5f146cd5f
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-11-16 20:30:46 -0700

    enhanced responsiveness, added px

--- components/Hero.js
+++ components/Hero.js
@@ -10,7 +10,7 @@ export const Hero = () => {
       style={{ backgroundImage: `url(${landingBg.src})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
     >
       <div className="absolute inset-0 bg-black opacity-40"></div>
-      <div className="hero-content text-center relative z-10 ">
+      <div className="hero-content text-center relative z-10 xs:px-1 sm:px-2 sml:px-3">
         <h1 className="text-4xl mb-4">Discover the Beauty of Succulents</h1>
         <h2 className="text-lg mb-8">Transform your space with our handpicked collection of unique and low-maintenance succulent plants.</h2>
         <Link href="/products" className="cta-button bg-jungleGreen text-white py-2 px-4 rounded inline-block text-lg hover:bg-oliveGreen duration-300 cursor-pointer">Explore Our Collection</Link>
