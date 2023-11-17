061c03923e8d2290795ffee3947159d0fdee8560
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-11-17 15:25:41 -0700

    removed PhotoSlider component

--- app/page.js
+++ app/page.js
@@ -12,7 +12,6 @@ export default function Home() {
         <SectionOne />
         <SectionTwo />
         {/* <SectionThree /> */}
-        {/* <PhotoSlider /> */}
         {/* <SectionFour />  */}
       </main>
     </>
