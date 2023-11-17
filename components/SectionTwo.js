8c24dc12c8fe390e16a39d68562b0864226c32bd
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-11-16 21:29:51 -0700

    enhanced responsiveness

--- components/SectionTwo.js
+++ components/SectionTwo.js
@@ -1,28 +1,30 @@
 import React from 'react'
 import { TbTruckDelivery } from 'react-icons/tb'
+import { PiPlantFill } from "react-icons/pi";
+import { BiSolidBookHeart } from "react-icons/bi";
 
 export const SectionTwo = () => {
   return (
-    <section className='py-10 -mt-[500px] h-80 bg-darkPurple flex gap-28 items-center justify-center mb-16'>
-      <div className='max-w-[1280px] flex justify-center items-center gap-52'>
-        <div className='w-52 h-52 bg-white rounded-full p-4 flex items-center justify-center'>
+    <section className='py-10 min-h-80 bg-darkPurple mdl:flex mdl:items-center mdl:justify-center'>
+      <div className='max-w-contentContainer mx-auto w-fit mdl:flex mdl:justify-center mdl:items-center mdl:gap-10 lg:gap-20 lgl:gap-24 xl:gap-28 xxl:gap-32'>
+        <div className='w-52 h-52 bg-white rounded-full p-4 my-5 flex items-center justify-center'>
           <div className='flex flex-col items-center gap-2'>
-            <TbTruckDelivery className='text-[30px]' />
-            <p>Fast delivery</p>
+          <PiPlantFill className='text-[50px]' />
+            <p className='font-semibold'>Locally sourced</p>
           </div>
         </div>
 
-        <div className='w-52 h-52 bg-white rounded-full p-4 flex items-center justify-center'>
+        <div className='w-52 h-52 bg-white rounded-full p-4 my-10 flex items-center justify-center'>
           <div className='flex flex-col items-center gap-2'>
-            <TbTruckDelivery />
-            <p>Fast delivery</p>
+            <TbTruckDelivery className='text-[50px]' />
+            <p className='font-semibold'>Fast delivery</p>
           </div>
         </div>
 
-        <div className='w-52 h-52 bg-white rounded-full p-4 flex items-center justify-center'>
+        <div className='w-52 h-52 bg-white rounded-full p-4 my-5 flex items-center justify-center'>
           <div className='flex flex-col items-center gap-2'>
-            <TbTruckDelivery />
-            <p>Fast delivery</p>
+          <BiSolidBookHeart className='text-[50px] ' />
+            <p className='font-semibold'>Expert Advice</p>
           </div>
         </div>
       </div>
