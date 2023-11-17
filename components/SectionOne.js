4432935b3070ab96f0fd15c13bf02169b0a9c7fd
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-11-17 14:11:49 -0700

    enhanced responsiveness

--- components/SectionOne.js
+++ components/SectionOne.js
@@ -2,15 +2,16 @@ import React from 'react'
 
 export const SectionOne = () => {
   return (
-    <div className='max-w-contentConatiner mx-auto bg-white mb-80 z-10 relative'>
+    <div className='max-w-contentConatiner mx-auto bg-white mb-10 md:mb-8 md:px-6 z-10 relative'>
       <div className='flex flex-col items-center gap-4'>
-        <h1 className='text-[32px] bg-lavender font-semibold text-white w-[500px] py-2 text-center -mt-[35px]'>
+        <h1 className='text-lg sml:text-xl md:text-2xl px-6 py-2 bg-lavender font-semibold rounded text-white text-center -mt-[20px]'>
           Discover Nature's Bounty
         </h1>
-        <span className='w-20 h-[3px] bg-jungleGreen'></span>
-        <p className='max-w-[800px] text-oliveGreen text-center text-base font-semibold'>
+        
+        <p className='max-w-contentContainer text-greyGreen text-center text-base font-semibold px-6'>
         Welcome to our botanical haven, where we invite you to Discover Nature's Bounty. Immerse yourself in the lush world of plants and greenery as we celebrate the beauty and vitality of the natural kingdom. Our plant shop is more than just a place to find your favorite greens—it's an exploration of the wonders of nature. From expertly curated collections to eco-conscious practices, we are dedicated to providing a verdant experience that goes beyond the ordinary. Join us on this journey of discovery, and let the abundance of nature inspire and enrich your life.
         </p>
+        <span className='w-20 min-h-[3px] bg-jungleGreen'></span>
       </div>
     </div>
   )
