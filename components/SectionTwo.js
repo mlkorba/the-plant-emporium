55df7f30f56b4eeaaf5a6f8aff6f39529ffa5b85
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-11-17 15:42:51 -0700

    changed section bg to lavender

--- components/SectionTwo.js
+++ components/SectionTwo.js
@@ -5,7 +5,7 @@ import { BiSolidBookHeart } from "react-icons/bi";
 
 export const SectionTwo = () => {
   return (
-    <section className='py-10 min-h-80 bg-darkPurple md:flex md:items-center md:justify-center'>
+    <section className='py-10 min-h-80 bg-lavender md:flex md:items-center md:justify-center'>
       <div className='max-w-contentContainer mx-auto w-fit md:flex md:justify-center md:items-center md:gap-5 mdl:gap-10 lg:gap-20 lgl:gap-24 xl:gap-28 xxl:gap-32'>
         <div className='w-40 h-40 sml:w-44 sml:h-44 lg:w-52 lg:h-52 bg-white rounded-full p-4 my-5 flex items-center justify-center drop-shadow-xl'>
           <div className='flex flex-col items-center gap-2 '>
