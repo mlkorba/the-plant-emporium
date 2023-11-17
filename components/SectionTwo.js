411888ba7538dd7aff38ab90870da3dc70d5fbc7
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-11-17 14:12:01 -0700

    enhanced responsiveness

--- components/SectionTwo.js
+++ components/SectionTwo.js
@@ -5,26 +5,26 @@ import { BiSolidBookHeart } from "react-icons/bi";
 
 export const SectionTwo = () => {
   return (
-    <section className='py-10 min-h-80 bg-darkPurple mdl:flex mdl:items-center mdl:justify-center'>
-      <div className='max-w-contentContainer mx-auto w-fit mdl:flex mdl:justify-center mdl:items-center mdl:gap-10 lg:gap-20 lgl:gap-24 xl:gap-28 xxl:gap-32'>
-        <div className='w-52 h-52 bg-white rounded-full p-4 my-5 flex items-center justify-center'>
-          <div className='flex flex-col items-center gap-2'>
-          <PiPlantFill className='text-[50px]' />
-            <p className='font-semibold'>Locally sourced</p>
+    <section className='py-10 min-h-80 bg-darkPurple md:flex md:items-center md:justify-center'>
+      <div className='max-w-contentContainer mx-auto w-fit md:flex md:justify-center md:items-center md:gap-5 mdl:gap-10 lg:gap-20 lgl:gap-24 xl:gap-28 xxl:gap-32'>
+        <div className='w-40 h-40 sml:w-44 sml:h-44 lg:w-52 lg:h-52 bg-white rounded-full p-4 my-5 flex items-center justify-center drop-shadow-xl'>
+          <div className='flex flex-col items-center gap-2 '>
+          <PiPlantFill className='text-4xl' />
+            <p className='text-base font-semibold'>Locally sourced</p>
           </div>
         </div>
 
-        <div className='w-52 h-52 bg-white rounded-full p-4 my-10 flex items-center justify-center'>
+        <div className='w-40 h-40 sml:w-44 sml:h-44 lg:w-52 lg:h-52 bg-white rounded-full p-4 my-10 flex items-center justify-center drop-shadow-xl'>
           <div className='flex flex-col items-center gap-2'>
-            <TbTruckDelivery className='text-[50px]' />
-            <p className='font-semibold'>Fast delivery</p>
+            <TbTruckDelivery className='text-4xl' />
+            <p className='text-base font-semibold'>Fast delivery</p>
           </div>
         </div>
 
-        <div className='w-52 h-52 bg-white rounded-full p-4 my-5 flex items-center justify-center'>
+        <div className='w-40 h-40 sml:w-44 sml:h-44 lg:w-52 lg:h-52 bg-white rounded-full p-4 my-5 flex items-center justify-center drop-shadow-xl'>
           <div className='flex flex-col items-center gap-2'>
-          <BiSolidBookHeart className='text-[50px] ' />
-            <p className='font-semibold'>Expert Advice</p>
+          <BiSolidBookHeart className='text-4xl' />
+            <p className='text-base font-semibold'>Expert Advice</p>
           </div>
         </div>
       </div>
