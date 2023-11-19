194fffaddec867be05e09a1426b77e2657afc235
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-11-19 16:51:12 -0700

    enhanced section two responsiveness

--- components/SectionTwo.js
+++ components/SectionTwo.js
@@ -9,21 +9,21 @@ export const SectionTwo = () => {
       <div className='max-w-contentContainer mx-auto w-fit md:flex md:justify-center md:items-center md:gap-5 mdl:gap-10 lg:gap-20 lgl:gap-24 xl:gap-28 xxl:gap-32'>
         <div className='w-40 h-40 sml:w-44 sml:h-44 lg:w-52 lg:h-52 bg-white rounded-full p-4 my-5 flex items-center justify-center drop-shadow-xl'>
           <div className='flex flex-col items-center gap-2 '>
-          <PiPlantFill className='text-4xl' />
+          <PiPlantFill className='text-4xl lg:text-5xl' />
             <p className='text-base font-semibold'>Locally sourced</p>
           </div>
         </div>
 
         <div className='w-40 h-40 sml:w-44 sml:h-44 lg:w-52 lg:h-52 bg-white rounded-full p-4 my-10 flex items-center justify-center drop-shadow-xl'>
           <div className='flex flex-col items-center gap-2'>
-            <TbTruckDelivery className='text-4xl' />
+            <TbTruckDelivery className='text-4xl lg:text-5xl' />
             <p className='text-base font-semibold'>Fast delivery</p>
           </div>
         </div>
 
         <div className='w-40 h-40 sml:w-44 sml:h-44 lg:w-52 lg:h-52 bg-white rounded-full p-4 my-5 flex items-center justify-center drop-shadow-xl'>
           <div className='flex flex-col items-center gap-2'>
-          <BiSolidBookHeart className='text-4xl' />
+          <BiSolidBookHeart className='text-4xl lg:text-5xl' />
             <p className='text-base font-semibold'>Expert Advice</p>
           </div>
         </div>
