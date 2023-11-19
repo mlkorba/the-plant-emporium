692fcba0cd8f74c5283763d4ac28d8aebca1232a
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-11-19 16:50:27 -0700

    enhanced section one

--- components/SectionFour.js
+++ components/SectionFour.js
@@ -4,13 +4,20 @@ import { succulentCorner } from '@/public/assets/images'
 
 export const SectionFour = () => {
   return (
-    <div className="relative">
-  <Image src={succulentCorner} className="w-full" />
-  <div className="absolute top-10 left-5 p-4 text-jungleGreen">
-    <h1 className="text-2xl text-center mb-10">Thank you for bringing a bit of greenery into your life with The Plant Emporium!</h1>
-    <p className='w-1/2'>Your choice to support local succulents means a lot to us and to our community. We're here to make your space a little greener, a little happier. Can't wait to see you again and help you find the perfect succulent companion. Until then, may your days be filled with the beauty of nature. Happy planting!</p>
-  </div>
-</div>
-
+    <div className='relative max-w-screen-sm mx-auto'>
+      <div className='relative'>
+        <h1 className='leading-5 text-lg px-4 font-semibold sml:text-2xl lg:text-3xl text-jungleGreen p-4 w-full md:w-3/4 absolute top-2 left-2 sml:top-10 sml:left-4 lg:top-24 lg:left-10'>
+          Thank you for choosing The Plant Emporium for your botancial needs!
+        </h1>
+        <p className='leading-4 text-sm px-5 sml:text-lg lg:text-2xl text-jungleGreen p-4 w-3/4 sml:w-1/2 xl:w-full absolute top-20 left-2 sm:top-16 sml:top-32 sml:left-4 lg:top-48 lg:left-10'>
+          Your support means the world to us!
+        </p>
+        <Image
+          src={succulentCorner}
+          className='w-full'
+          alt='Succulent Corner'
+        />
+      </div>
+    </div>
   )
 }
