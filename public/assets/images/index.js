8a7115c118fa40c9fff50f3673f9a87d2a18365e
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-11-19 16:51:34 -0700

    removed unused image

--- public/assets/images/index.js
+++ public/assets/images/index.js
@@ -1,6 +1,5 @@
 import logo from "./logo.png";
 import landingBg from "./landing-bg.webp";
-import plantOne from "./product-images/kari-shea-3_cyj5YkhTs-unsplash.jpg"
 import localPlants from "./local-plants.jpg"
 import succulentCorner from "./succulent-corner-bg.jpg"
 
@@ -8,7 +7,6 @@ import succulentCorner from "./succulent-corner-bg.jpg"
 export {
   logo,
   landingBg,
-  plantOne,
   localPlants,
   succulentCorner,
 };
