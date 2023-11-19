83b7a429695008e868e69f5ccd122da936eed22c
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-11-19 16:50:55 -0700

    enhanced section two responsiveness

--- components/SectionThree.js
+++ components/SectionThree.js
@@ -4,11 +4,15 @@ import { localPlants } from '@/public/assets/images'
 
 export const SectionThree = () => {
   return (
-    <section className='max-w-contentContainer mx-auto p-5'>
-      <div className='md:flex md:items-center md:justify-center md:gap-16 mb-5'>
-        <h1 className='text-xl font-semibold text-center mb-4'>Plant Care Tips</h1>
-        <div>
-          <p className='text-sm rounded-xl mb-5'>
+    <section className='max-w-contentContainer mx-auto my-10 px-10'>
+      <div className='md:flex md:items-center md:justify-center md:gap-8'>
+        <h1 className='text-2xl lg:text-3xl font-semibold text-center mb-4 md:w-full'>
+          Plant Care Tips
+        </h1>
+      </div>
+      <div className='md:flex md:items-center md:justify-center md:gap-8 mb-5'>
+        <div className='md:w-full xl:w-1/2'>
+          <p className='text-sm lg:text-lg rounded-xl mb-2'>
             Veniam mollit officia et ipsum eiusmod occaecat cupidatat culpa
             aute. Adipisicing quis anim excepteur nostrud enim aliqua duis id
             sunt velit labore. Sint tempor laboris ex laborum. Aliqua nostrud in
@@ -17,25 +21,14 @@ export const SectionThree = () => {
             labore. Nostrud non minim quis voluptate. Esse ullamco irure laborum
             mollit.
           </p>
-        </div>
-        <div>
-          <Image src={localPlants} className='text-sm rounded-xl shadow-md' />
         </div>
       </div>
-      <div className='md:flex md:items-center md:justify-center md:gap-16 mb-5'>
-      <div>
-          <p className='text-sm rounded-xl mb-5'>
-            Veniam mollit officia et ipsum eiusmod occaecat cupidatat culpa
-            aute. Adipisicing quis anim excepteur nostrud enim aliqua duis id
-            sunt velit labore. Sint tempor laboris ex laborum. Aliqua nostrud in
-            ad excepteur reprehenderit sunt nisi id non labore cupidatat laboris
-            sit sit. Consectetur aliqua non nulla magna ea deserunt irure ea
-            labore. Nostrud non minim quis voluptate. Esse ullamco irure laborum
-            mollit.
-          </p>
-        </div>
-        <div>
-          <Image src={localPlants} className='text-sm rounded-xl shadow-md' />
+      <div className='md:flex md:items-center md:justify-center md:gap-8 mb-5'>
+        <div className='md:w-full xl:w-1/2'>
+          <Image
+            src={localPlants}
+            className='w-full h-auto text-sm rounded-xl shadow-md'
+          />
         </div>
       </div>
     </section>
