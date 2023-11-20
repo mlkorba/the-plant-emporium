12228495c731c99fb866d646050f2ebf5513ea41
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-11-19 18:31:40 -0700

    enhanced product page responsiveness

--- app/products/page.js
+++ app/products/page.js
@@ -14,8 +14,9 @@ async function ProductsPage () {
 
   return (
     <>
-      <main className='p-10'>
-        <div className='grid grid-cols-3 max-w-screen w-contain'>
+      <main className='p-3 md:p-4 lg:p-5 lgl:p-7 xl:p-8'>
+      <h1 className='text-2xl lgl:text-4xl text-center text-jungleGreen font-bold pt-4 mb-4'>Products</h1>
+        <div className='sml:grid sml:grid-cols-2 mdl:grid-cols-3 max-w-contentContainer mx-auto w-contain'>
           {values.map(item => (
             <ProductCard key={item.uid} products={item} />
           ))}
