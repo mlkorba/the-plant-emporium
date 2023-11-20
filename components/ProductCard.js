de1b4884a24d9080843ab1115a28cd2545e45c59
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-11-19 18:30:01 -0700

    enhanced ProductCard responsiveness

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -5,32 +5,35 @@ import Link from 'next/link'
 export const ProductCard = ({ uid, products }) => {
   const imageURL = `/assets/images/product-images/${products.image}`
   return (
-    <div className='flex justify-center flex-wrap mb-10'> 
-  <div className='group w-3/4 h-full mx-2 my-2 rounded-lg shadow-lg overflow-hidden'>
-    <div className='w-full cursor-pointer overflow-hidden mb-10'>
+    <div className='mb-5 lg:mb-10 px-2'>
+  <div className='w-full sml:w-full m-2 rounded-lg shadow-lg overflow-hidden'>
+    <div className='w-full cursor-pointer overflow-hidden mb-5'>
       <div>
         <Image
-          className='object-cover w-full h-[500px] group-hover:scale-125 transition duration-500 cursor-pointer'
+          className='object-cover w-full h-[320px] lg:h-[400px] lgl:h-[450px] xl:h-[500px] group-hover:scale-125 transition duration-500 cursor-pointer'
           src={imageURL}
-          width={500}
-          height={500}
+          width={320}  // Adjusted width for smaller screens
+          height={250} // Adjusted height for smaller screens
           alt={products.plant_name}
         />
       </div>
     </div>
-    <div className='relative w-full h-[250px] bg-white'>
-      <div className='text-center'>
+    <div className='relative w-full h-[250px] sml:h-[265px] lgl:h-[300px] bg-white p-2 lg:p-5 lgl:px-8 '>
+      <div >
         <div>
-          <h2 className='text-base font-bold'>{products.plant_name}</h2>
+          <h2 className='text-sm md:text-base text-center font-bold mb-2 text-jungleGreen'>{products.plant_name}</h2>
         </div>
-        <div className='text-center mb-5'>
+        <div className='text-sm px-2 md:text-base text-jungleGreen'>
+          <p className='mb-5'>{products.description}</p> 
+        </div>
+        <div className='sml:absolute sml:bottom-20 sml:left-8 sml:w-3/4 mb-2'>
+
+        <div className='text-center sml:flex sml:justify-between text-sm md:text-base mb-2 text-jungleGreen '>
           <p className='font-semibold'>${products.price}</p>
           <p className='line-through'>${products.oldPrice}</p>
         </div>
-        <div className='text-center px-5'>
-          <p className='mb-5'>{products.description}</p> 
         </div>
-        <div className='text-center absolute bottom-4 left-1/2 transform -translate-x-1/2 mb-5'>
+        <div className='text-center md:text-base mb-10 absolute bottom-0 left-0 w-full '>
           <Link href={`/products/${uid}`}>
             <span className='bg-oliveGreen hover:bg-jungleGreen text-white font-bold py-2 px-4 rounded-full cursor-pointer'>
               View Details
@@ -41,6 +44,8 @@ export const ProductCard = ({ uid, products }) => {
     </div>
   </div>
 </div>
+
+
 
   )
 }
