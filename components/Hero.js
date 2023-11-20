8092a2e74e0261920dc90e35f7b4a63decc47a86
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-11-19 19:08:23 -0700

    enhanced responsiveness

--- components/Hero.js
+++ components/Hero.js
@@ -11,9 +11,9 @@ export const Hero = () => {
     >
       <div className="absolute inset-0 bg-black opacity-40"></div>
       <div className="hero-content text-center relative z-10 px-5 sml:px-6 md:px-8">
-        <h1 className="text-4xl mb-4">Discover the Beauty of Succulents</h1>
-        <h2 className="text-lg mb-8">Transform your space with our handpicked collection of unique and low-maintenance succulent plants.</h2>
-        <Link href="/products" className="cta-button bg-jungleGreen text-white py-2 px-4 rounded-full inline-block text-lg drop-shadow-md hover:bg-oliveGreen duration-300 cursor-pointer">Explore Our Collection</Link>
+        <h1 className="text-4xl lgl:text-5xl mb-4">Discover the Beauty of Succulents</h1>
+        <h2 className="text-lg lgl:text-2xl mb-8">Transform your space with our handpicked collection of unique and low-maintenance succulent plants.</h2>
+        <Link href="/products" className="cta-button bg-jungleGreen text-white py-2 px-4 rounded-full inline-block text-lg lgl:text-2xl drop-shadow-md hover:bg-oliveGreen duration-300 cursor-pointer">Explore Our Collection</Link>
       </div>
     </div>
   )
