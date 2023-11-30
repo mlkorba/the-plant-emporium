a9b1501b885298e4f41d4454273fb12babc96f57
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-11-30 12:37:43 -0700

    adjusted design

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -6,46 +6,47 @@ export const ProductCard = ({ uid, products }) => {
   const imageURL = `/assets/images/product-images/${products.image}`
   return (
     <div className='mb-5 lg:mb-10 px-2'>
-  <div className='w-full sml:w-full m-2 rounded-lg shadow-lg overflow-hidden'>
-    <div className='w-full cursor-pointer overflow-hidden mb-5'>
-      <div>
-        <Image
-          className='object-cover w-full h-[320px] lg:h-[400px] lgl:h-[450px] xl:h-[500px] group-hover:scale-125 transition duration-500 cursor-pointer'
-          src={imageURL}
-          width={320}  // Adjusted width for smaller screens
-          height={250} // Adjusted height for smaller screens
-          alt={products.plant_name}
-        />
+      <div className='w-full sml:w-full m-2 rounded-lg shadow-lg overflow-hidden'>
+        <div className='w-full cursor-pointer overflow-hidden mb-5'>
+          <div>
+            <Image
+              className='object-cover object-bottom w-full  h-[320px] lg:h-[400px] lgl:h-[450px] xl:h-[500px] transition duration-500 cursor-pointer hover:scale-110'
+              src={imageURL}
+              width={320} // Adjusted width for smaller screens
+              height={250} // Adjusted height for smaller screens
+              alt={products.plant_name}
+            />
+          </div>
+        </div>
+        {/* Description */}
+        <div className='relative w-full h-[250px] sml:h-[265px] lgl:h-[300px] bg-white p-2 lg:p-5 lgl:px-8'>
+          <div className='flex flex-col'>
+            <div>
+              <h2 className='text-sm md:text-base text-center font-bold mb-2 text-jungleGreen'>
+                {products.plant_name}
+              </h2>
+            </div>
+            <div className='px-4'>
+              <div className='text-sm md:text-base text-jungleGreen'>
+                <p>{products.description}</p>
+              </div>
+              <div className='absolute bottom-24 left-10 right-10'>
+                <div className='flex justify-between text-sm md:text-base text-jungleGreen'>
+                  <p className='font-bold'>${products.price}</p>
+                  <p className='line-through'>${products.oldPrice}</p>
+                </div>
+              </div>
+              <div className='text-center md:text-base absolute bottom-10 left-0 w-full'>
+                <Link href={`/products/${uid}`}>
+                  <span className='bg-oliveGreen hover:bg-jungleGreen text-white font-bold py-2 px-4 rounded-full cursor-pointer'>
+                    View Details
+                  </span>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-    <div className='relative w-full h-[250px] sml:h-[265px] lgl:h-[300px] bg-white p-2 lg:p-5 lgl:px-8 '>
-      <div >
-        <div>
-          <h2 className='text-sm md:text-base text-center font-bold mb-2 text-jungleGreen'>{products.plant_name}</h2>
-        </div>
-        <div className='text-sm px-2 md:text-base text-jungleGreen'>
-          <p className='mb-5'>{products.description}</p> 
-        </div>
-        <div className='sml:absolute sml:bottom-20 sml:left-8 sml:w-3/4 mb-2'>
-
-        <div className='text-center sml:flex sml:justify-between text-sm md:text-base mb-2 text-jungleGreen '>
-          <p className='font-semibold'>${products.price}</p>
-          <p className='line-through'>${products.oldPrice}</p>
-        </div>
-        </div>
-        <div className='text-center md:text-base mb-10 absolute bottom-0 left-0 w-full '>
-          <Link href={`/products/${uid}`}>
-            <span className='bg-oliveGreen hover:bg-jungleGreen text-white font-bold py-2 px-4 rounded-full cursor-pointer'>
-              View Details
-            </span>
-          </Link>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-
-
-
   )
 }
