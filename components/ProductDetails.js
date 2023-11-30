1d2e3a0bdda789b91617417582fc84c057a6033a
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-11-30 14:31:49 -0700

    continued designing single product page

--- components/ProductDetails.js
+++ components/ProductDetails.js
@@ -5,31 +5,61 @@ import { FaRulerCombined } from 'react-icons/fa'
 export const ProductDetails = ({ product }) => {
   const imageURL = `/assets/images/product-images/${product.image}`
   return (
-    <div className='max-w-contentContainer mx-auto'>
-      <div className='flex justify-center gap-10'>
+    <div className='max-w-contentContainer flex justify-center gap-10'>
+      <div>
         <div>
           <Image
             src={imageURL}
             alt={product.plant_name}
             width={500}
             height={500}
+            className=''
           />
         </div>
-        <div>
-          <h1 className='text-4xl font-bold'>{product.plant_name}</h1>
-          <p>${product.price}</p>
-          <p className='line-through'>${product.oldPrice}</p>
-
-          <div className='mb-5'>
-            <h2 className='font-semibold'>Description</h2>
-            <p>{product.description}</p>
-            <p>{product.plant_family}</p>
-            <p>{product.plant_scientific_name}</p>
+        <div className='py-8 px-4 sml:p-8'>
+          <div className='text-center'>
+            <h1 className='text-xl sm:text-2xl sml:text-3xl font-bold'>
+              {product.plant_name}
+            </h1>
+            <span>
+              {product.in_stock ? (
+                <>
+                  <div className='flex items-center justify-center gap-4'>
+                    <p className='text-lg font-bold'>${product.price}</p>
+                    <p className='line-through'>${product.oldPrice}</p>
+                  </div>
+                </>
+              ) : (
+                <p className='text-red-500 font-bold text-base sm:text-lg sml:text-xl'>
+                  Out of Stock
+                </p>
+              )}
+            </span>
+            <span className='w-20 min-h-[5px] bg-jungleGreen'></span>
           </div>
 
-          <div>
-            <h2 className='font-semibold'>Plant Information</h2>
-            <ul>
+          <div className='text-sm p-2 mb-2'>
+            <h2 className='text-lg sm:text-xl sml:text-2xl font-bold mb-2'>
+              Description
+            </h2>
+            <div className='text-sm sm:text-base'>
+              <p className='mb-2'>{product.description}</p>
+              <p className='mb-2'>
+                <span className='font-semibold'>Plant Family: </span>
+                {product.plant_family}
+              </p>
+              <p>
+                <span className='font-semibold'>Plant Scientific Name: </span>
+                {product.plant_scientific_name}
+              </p>
+            </div>
+          </div>
+
+          <div className='text-sm mb-4 border-solid text-white bg-darkPurple p-4 item rounded-xl'>
+            <h2 className='text-lg sm:text-xl sml:text-2xl font-semibold mb-2  '>
+              Plant Information
+            </h2>
+            <ul className='flex flex-col gap-1 text-sm sm:text-base'>
               <li>
                 <span className='font-semibold'>Height:</span> {product.height}{' '}
                 cm
@@ -51,13 +81,10 @@ export const ProductDetails = ({ product }) => {
               </li>
             </ul>
           </div>
-          <div className='flex justify-between'>
-            <button className='bg-green-500 text-white py-2 px-4 rounded cursor-pointer'>
+          <div className='text-center px-2'>
+            <button className='bg-jungleGreen hover:bg-oliveGreen text-white p-4 rounded-full cursor-pointer w-full font-semibold'>
               Add to Cart
             </button>
-            <span className='font-bold'>
-              {product.in_stock ? 'In Stock' : 'Out of Stock'}
-            </span>
           </div>
         </div>
       </div>
