85b26836e163e4c07f1d76e3198a9d0975c025e7
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-11-30 12:36:22 -0700

    removed unused imports

--- app/products/page.js
+++ app/products/page.js
@@ -1,9 +1,5 @@
-import Link from 'next/link'
-import Image from 'next/image'
 import { getProducts } from '@/lib/firebase/getProducts'
 import { ProductCard } from '@/components/ProductCard'
-import { Navbar } from '@/components/Navbar'
-import { Footer } from '@/components/Footer'
 
 async function ProductsPage () {
   const payload = await getProducts()
@@ -15,10 +11,12 @@ async function ProductsPage () {
   return (
     <>
       <main className='p-3 md:p-4 lg:p-5 lgl:p-7 xl:p-8'>
-      <h1 className='text-2xl lgl:text-4xl text-center text-jungleGreen font-bold pt-4 mb-4'>Products</h1>
+        <h1 className='text-2xl lgl:text-4xl text-center text-jungleGreen font-bold pt-4 mb-4'>
+          Products
+        </h1>
         <div className='sml:grid sml:grid-cols-2 mdl:grid-cols-3 max-w-contentContainer mx-auto w-contain'>
           {values.map(item => (
-            <ProductCard key={item.uid} products={item} />
+            <ProductCard uid={item.uid} products={item} />
           ))}
         </div>
       </main>
