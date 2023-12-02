cb2da8b39f29ece54dbe5ad64e7513030446fe32
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-12-01 22:10:06 -0700

    added page extensions

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['source.unsplash.com'],
-      },
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx', 'page.js'],
+  images: {
+    domains: ['source.unsplash.com']
+  }
 }
 
 module.exports = nextConfig
