a7a0fe524e71280e16eef3ac7c15161c205d8cd0
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-12-01 22:08:48 -0700

    enhanced responsiveness

--- components/ProductDetails.js
+++ components/ProductDetails.js
@@ -5,61 +5,56 @@ import { FaRulerCombined } from 'react-icons/fa'
 export const ProductDetails = ({ product }) => {
   const imageURL = `/assets/images/product-images/${product.image}`
   return (
-    <div className='max-w-contentContainer flex justify-center gap-10'>
-      <div>
+    <div className='max-w-contentContainer lgl:mx-auto'>
+      <div className='lgl:flex lgl:items-center lgl:justify-center lgl:gap-10 lgl:my-5 '>
         <div>
           <Image
             src={imageURL}
             alt={product.plant_name}
-            width={500}
-            height={500}
-            className=''
+            width={320}
+            height={200}
+            className='w-full h-auto mx-auto sml:w-[75%] md:w-[60%] mdl:w-[40%] lgl:w-[100%]'
           />
         </div>
-        <div className='py-8 px-4 sml:p-8'>
-          <div className='text-center'>
-            <h1 className='text-xl sm:text-2xl sml:text-3xl font-bold'>
-              {product.plant_name}
-            </h1>
+        <div className='mx-auto p-8 md:w-[75%] lgl:w-[50%] lgl:mx-0'>
+          <div className='text-center text-jungleGreen mb-4'>
+            <h1 className='text-2xl font-bold'>{product.plant_name}</h1>
             <span>
               {product.in_stock ? (
                 <>
-                  <div className='flex items-center justify-center gap-4'>
-                    <p className='text-lg font-bold'>${product.price}</p>
-                    <p className='line-through'>${product.oldPrice}</p>
+                  <div className='flex items-center justify-center gap-4 text-lg text-jungleGreen'>
+                    <p className='font-bold text-base'>${product.price}</p>
+                    <p className='line-through text-base'>
+                      ${product.oldPrice}
+                    </p>
                   </div>
                 </>
               ) : (
-                <p className='text-red-500 font-bold text-base sm:text-lg sml:text-xl'>
-                  Out of Stock
-                </p>
+                <p className='text-red-500 font-bold text-base'>Out of Stock</p>
               )}
             </span>
-            <span className='w-20 min-h-[5px] bg-jungleGreen'></span>
           </div>
 
-          <div className='text-sm p-2 mb-2'>
-            <h2 className='text-lg sm:text-xl sml:text-2xl font-bold mb-2'>
-              Description
-            </h2>
-            <div className='text-sm sm:text-base'>
-              <p className='mb-2'>{product.description}</p>
-              <p className='mb-2'>
+          <div className='text-sm p-2 mb-4 border-t-2 border-darkPurple text-jungleGreen '>
+            <h2 className='text-xl font-semibold '>Description</h2>
+            <div className=' text-sm'>
+              <p className='text-base mb-4'>{product.description}</p>
+              <p className='text-base  mb-4'>
                 <span className='font-semibold'>Plant Family: </span>
                 {product.plant_family}
               </p>
-              <p>
+              <p className='text-base '>
                 <span className='font-semibold'>Plant Scientific Name: </span>
                 {product.plant_scientific_name}
               </p>
             </div>
           </div>
 
-          <div className='text-sm mb-4 border-solid text-white bg-darkPurple p-4 item rounded-xl'>
-            <h2 className='text-lg sm:text-xl sml:text-2xl font-semibold mb-2  '>
+          <div className='text-sm mb-8 border-solid item rounded-2xl border-2 border-darkPurple sml:w-[75%] mdl:w-[50%] lgl:w-[80%] mx-auto'>
+            <h2 className='text-lg text-white bg-darkPurple px-4 py-2 rounded-t-xl'>
               Plant Information
             </h2>
-            <ul className='flex flex-col gap-1 text-sm sm:text-base'>
+            <ul className='flex flex-col gap-1 p-3 rounded-b-xl text-darkPurple text-base '>
               <li>
                 <span className='font-semibold'>Height:</span> {product.height}{' '}
                 cm
@@ -81,8 +76,11 @@ export const ProductDetails = ({ product }) => {
               </li>
             </ul>
           </div>
+
           <div className='text-center px-2'>
-            <button className='bg-jungleGreen hover:bg-oliveGreen text-white p-4 rounded-full cursor-pointer w-full font-semibold'>
+            <button
+              className={`text-base bg-jungleGreen hover:bg-oliveGreen text-white py-2 px-4 rounded-full cursor-pointer w-full sml:w-[75%] mdl:w-[50%] font-semibold`}
+            >
               Add to Cart
             </button>
           </div>
