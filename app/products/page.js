05fcb1372b2a8bdce44431522d7719e403a10332
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-12-01 23:00:30 -0700

    changed h1 from Products to Plants

--- app/products/page.js
+++ app/products/page.js
@@ -11,10 +11,10 @@ async function ProductsPage () {
   return (
     <>
       <main className='p-3 md:p-4 lg:p-5 lgl:p-7 xl:p-8'>
-        <h1 className='text-2xl lgl:text-4xl text-center text-jungleGreen font-bold pt-4 mb-4'>
-          Products
+        <h1 className='text-2xl lgl:text-4xl text-center text-jungleGreen font-bold pt-4 mb-10'>
+          Plants
         </h1>
-        <div className='sml:grid sml:grid-cols-2 mdl:grid-cols-3 max-w-contentContainer mx-auto w-contain'>
+        <div className='sml:grid sml:grid-cols-2 sml:gap-5 mdl:grid-cols-3 max-w-contentContainer mx-auto w-contain'>
           {values.map(item => (
             <ProductCard uid={item.uid} products={item} />
           ))}
