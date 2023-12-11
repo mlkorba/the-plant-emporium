8541474537a2bb619018400aca0c48361757edf3
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-12-10 17:19:57 -0700

    added min-h-screen

--- components/SectionFour.js
+++ components/SectionFour.js
@@ -1,22 +1,25 @@
 import React from 'react'
-import Image from 'next/image'
 import { succulentCorner } from '@/public/assets/images'
 
 export const SectionFour = () => {
   return (
-    <div className='relative max-w-screen-sm mx-auto'>
-      <div className='relative'>
-        <h1 className='leading-5 p-5 text-lg font-semibold sml:text-2xl lg:text-3xl lgl:text-[65px] lgl:leading-tight text-jungleGreen w-full md:w-3/4 absolute top-2 left-0 sml:top-10 sml:left-4 lg:top-24 lg:left-10'>
-          Thank you for choosing The Plant Emporium for your botancial needs!
+    <div
+      className='relative min-h-screen flex flex-col justify-center text-white z-0 bg-fixed'
+      style={{
+        backgroundImage: `url(${succulentCorner.src})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center'
+      }}
+    >
+      <div className='absolute inset-0 bg-black opacity-30'></div>
+      <div className='text-left relative z-10 p-8 md:p-12 mdl:p-16 lgl:px-32 xl:p-48 '>
+        <h1 className='text-xl sml:text-2xl mdl:text-3xl font-semibold mb-4'>
+          Thank you for choosing The Plant Emporium for your botanical needs!
         </h1>
-        <p className='leading-4 p-5 w-[60%] sml:w-[55%]  text-jungleGreen text-sm sml:text-xl lg:text-2xl lgl:text-[50px] lgl:leading-tight absolute top-20 left-0 sm:top-16 sml:top-32 sml:left-4 lg:top-48 lg:left-10 lgl:top-96'>
-          Your support means the world to us!
-        </p>
-        <Image
-          src={succulentCorner}
-          className='w-full'
-          alt='Succulent Corner'
-        />
+        <h2 className='text-base sml:text-lg mdl:text-xl lgl:text-2xl '>
+          Your support means the world to us, and we hope that the green
+          companions you've selected bring joy and freshness to your space.
+        </h2>
       </div>
     </div>
   )
