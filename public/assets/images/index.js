e50244ddb89ae83cb77b66bb1d6e634b2d63f762
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-12-15 23:55:54 -0700

    added sad plant image

--- public/assets/images/index.js
+++ public/assets/images/index.js
@@ -1,12 +1,7 @@
-import logo from "./logo.png";
-import landingBg from "./landing-bg.webp";
-import localPlants from "./local-plants.jpg"
-import succulentCorner from "./succulent-corner-bg.jpg"
+import logo from './logo.png'
+import landingBg from './landing-bg.webp'
+import localPlants from './local-plants.jpg'
+import succulentCorner from './succulent-corner-bg.jpg'
+import sadPlant from './sad-plant.jpg'
 
-
-export {
-  logo,
-  landingBg,
-  localPlants,
-  succulentCorner,
-};
+export { logo, landingBg, localPlants, succulentCorner, sadPlant }
