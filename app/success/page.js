1acd2b19ef106216d0cca8201c36f343160047cf
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-12-16 00:23:25 -0700

    added hanging plant image

--- app/success/page.js
+++ app/success/page.js
@@ -1,13 +1,13 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import { sadPlant } from '@/public/assets/images'
+import { hangingPlant } from '@/public/assets/images'
 
 export default function Success ({ children }) {
   return (
     <div className='max-w-contentContainer  mx-auto items-center p-20 py-40'>
-      <div className='flex items-center justify-center gap-10'>
+      <div>
         <div>
-          <Image src={sadPlant} className='' width={350} />
+          <Image src={hangingPlant} className='mx-auto' width={350} />
         </div>
         <div className='text-center'>
           <h1 className='text-2xl mb-5 bg-oliveGreen rounded-lg w-fit mx-auto px-5 py-2 text-red-50 font-medium'>
