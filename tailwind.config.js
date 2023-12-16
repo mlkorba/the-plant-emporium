5112912516ba80eaa0a43274fc60b94037ce3c8a
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2023-12-15 23:28:57 -0700

    added background Image to tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,8 +22,13 @@ module.exports = {
         xl: '1280px',
         xxl: '1400px'
       },
+      backgroundImage: {
+        succulents:
+          'url("https://images.unsplash.com/photo-1519289776718-d7becf12a9f0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")'
+      },
       colors: {
         oliveGreen: '#778A74',
+        medGreen: '#324A49',
         jungleGreen: '#183E33',
 
         greyGreen: '#566269',
