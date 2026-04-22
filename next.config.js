584748453f16ffd94e5441e32fdc6543af1adcd0
Marcia Korba <104874443+mlkorba@users.noreply.github.com>
2026-04-22 20:40:56 +0100

    Removed deprecation and updated

--- next.config.js
+++ next.config.js
@@ -2,7 +2,13 @@
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx', 'page.js'],
   images: {
-    domains: ['source.unsplash.com']
+    // domains: ['source.unsplash.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+      },
+    ],
   }
 }
 
